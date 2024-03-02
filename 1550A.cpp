1168a1e1b0d4959c72486cb23a1da0602a8048d5
Sadmankabir2084 <123572457+sadman2084@users.noreply.github.com>
2024-03-02 10:16:03 +0600

    Add files via upload

---
+++ 1550A.cpp
@@ -0,0 +1,36 @@
+// Author:Sadman kabir
+// date:
+//-------------------------------------------------------------
+#include <bits/stdc++.h>
+using namespace std;
+#define pb(x) push_back(x)
+#define No cout << "No" << endl
+#define Yes cout << "Yes" << endl
+#define YES cout << "YES" << endl
+#define NO cout << "NO" << endl
+#define mm cout << "-1" << endl
+#define nl "\n"
+#define rr return
+#define ll long long int
+//----------------------------------------------------------------
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int i = 1;
+        int count = 0;
+        int sum = 0;
+        while (sum < n)
+        {
+            count++;
+            sum = sum + i;
+            i = i + 2;
+        }
+        cout << count << "\n";
+    }
+}
