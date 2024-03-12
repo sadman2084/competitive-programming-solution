fddfedd3e9cb30555bbb502d36e24ddf6b5d06c0
Sadmankabir2084 <123572457+sadman2084@users.noreply.github.com>
2024-03-12 08:59:45 -0400

    Add files via upload

---
+++ 1589A.cpp
@@ -0,0 +1,32 @@
+// Author:Sadman kabir
+// date:
+//-------------------------------------------------------------
+#include <bits/stdc++.h>
+using namespace std;
+#define pb(x) push_back(x)
+#define No cout << "No" << endl
+#define Yes cout << "Yes" << endl
+#define YES cout << "YES" << endl
+#define NO cout << "NO" << endl
+#define mm cout << "-1" << endl
+#define nl "\n"
+#define rr return
+#define ll long long int
+//----------------------------------------------------------------
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll u, v;
+        cin >> u >> v;
+        ll a = v * v;
+        ll b = u * u;
+        a = -a;
+        cout << b << " " << a << endl;
+    }
+}
